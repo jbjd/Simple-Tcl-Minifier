8e2c6401ad34fd2b52fe8494cdb3d27374cd108d
jbjd <jimdemetris@yahoo.com>
2026-05-27 19:22:57 -0500

    Working setup script

--- setup.py
+++ setup.py
@@ -1,3 +1,30 @@
-import setuptools
+import os
 
-setuptools.setup()
+from setuptools import Extension, setup
+
+os.environ["CC"] = "gcc"
+
+PACKAGE: str = "personal_simple_tcl_minifier"
+C_FOLDER: str = f"{PACKAGE}/c_extensions"
+
+# This is stupid and I hate that I have to do it
+# pip would not accept envs or args I found online to set compiler
+if os.name == "nt" and not os.path.exists("setup.cfg"):
+    with open("setup.cfg", "w", encoding="utf-8") as fp:
+        fp.write("""[build]
+compiler=mingw32""")
+
+# Define the extension module
+parse_extension = Extension(
+    name=f"{PACKAGE}.parse",
+    sources=[
+        f"{C_FOLDER}/python_modules/parse.c",
+        f"{C_FOLDER}/tcl_parse.c",
+    ],
+    extra_compile_args=["-O3", "-Wall"],
+)
+
+setup(
+    ext_modules=[parse_extension],
+    exclude_package_data={"": ["*.c", "*.h"]},
+)
