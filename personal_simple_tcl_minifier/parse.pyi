d226937f9297b465e652b94b76a7de4ec96f97aa
jbjd <jimdemetris@yahoo.com>
2026-05-29 22:08:49 -0500

    Cleanup

--- personal_simple_tcl_minifier/parse.pyi
+++ personal_simple_tcl_minifier/parse.pyi
@@ -3,9 +3,3 @@ def tcl_minify(source: str) -> str:
 
     :param source: Tcl source code
     :returns: Minified version of source"""
-
-def tcl_minify_to_file(source: str, path: str) -> None:
-    """Minifies a tcl script and writes to path.
-
-    :param source: Tcl source code
-    :param path: Path to write to"""
