2bf610acabc10dec3299a52de4332fe64ce9eca5
James D <63110925+jbjd@users.noreply.github.com>
2026-06-01 21:08:37 -0500

    Refactor: Add clang-format (#7)

--- personal_simple_tcl_minifier/c_extensions/python_modules/parse.c
+++ personal_simple_tcl_minifier/c_extensions/python_modules/parse.c
@@ -1,19 +1,17 @@
 #define Py_LIMITED_API 0x030c0000
 #define PY_SSIZE_T_CLEAN
 
-#include <Python.h>
-
-#include <stdlib.h>
-
-#include "../includes/c_optimizations.h"
 #include "../includes/parse.h"
 
-static PyObject *Py_tcl_minify(PyObject *self, PyObject *arg)
-{
+#include "../includes/c_optimizations.h"
+
+#include <Python.h>
+#include <stdlib.h>
+
+static PyObject *Py_tcl_minify(PyObject *self, PyObject *arg) {
     Py_ssize_t size;
     const char *source = PyUnicode_AsUTF8AndSize(arg, &size);
-    if (unlikely(source == NULL))
-    {
+    if (unlikely(source == NULL)) {
         return NULL;
     }
 
@@ -29,10 +27,10 @@ static PyObject *Py_tcl_minify(PyObject *self, PyObject *arg)
 
 static PyMethodDef parse_methods[] = {
     {"tcl_minify", Py_tcl_minify, METH_O, NULL},
-    {NULL, NULL, 0, NULL}};
+    {NULL, NULL, 0, NULL},
+};
 
-static int parse_exec(PyObject *Py_UNUSED(module))
-{
+static int parse_exec(PyObject *Py_UNUSED(module)) {
     return 0;
 }
 
@@ -42,16 +40,17 @@ static PyModuleDef_Slot parse_slots[] = {
 #ifdef Py_GIL_DISABLED
     {Py_mod_gil, Py_MOD_GIL_NOT_USED},
 #endif
-    {0, NULL}};
+    {0, NULL},
+};
 
 static struct PyModuleDef parse_module = {
     PyModuleDef_HEAD_INIT,
     .m_name = "parse",
     .m_size = 0,
     .m_methods = parse_methods,
-    .m_slots = parse_slots};
+    .m_slots = parse_slots,
+};
 
-PyMODINIT_FUNC PyInit_parse(void)
-{
+PyMODINIT_FUNC PyInit_parse(void) {
     return PyModuleDef_Init(&parse_module);
 }
