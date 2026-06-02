17b721221825356b86f38e5398b27dc525c61703
jbjd <jimdemetris@yahoo.com>
2026-06-01 20:51:55 -0500

    Add clang-format

--- personal_simple_tcl_minifier/c_extensions/includes/parse.h
+++ personal_simple_tcl_minifier/c_extensions/includes/parse.h
@@ -1,6 +1,8 @@
 #ifndef PTCL_PARSE
 #define PTCL_PARSE
 
+#include <stddef.h>
+
 char *tcl_minify(const char *source, size_t size, size_t *size_out);
 
 #endif /* PTCL_PARSE */
--- personal_simple_tcl_minifier/c_extensions/parse.c
+++ personal_simple_tcl_minifier/c_extensions/parse.c
@@ -2,7 +2,6 @@
 
 #include <ctype.h>
 #include <stdbool.h>
-#include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
 
