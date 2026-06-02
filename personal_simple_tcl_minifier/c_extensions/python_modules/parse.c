56e141fda373ed99c81432ef208909f905808c3e
James D <63110925+jbjd@users.noreply.github.com>
2026-06-02 17:29:28 -0500

    Improvement: Add function to run on file (#8)

--- personal_simple_tcl_minifier/c_extensions/python_modules/parse.c
+++ personal_simple_tcl_minifier/c_extensions/python_modules/parse.c
@@ -6,7 +6,13 @@
 #include "../includes/c_optimizations.h"
 
 #include <Python.h>
+#include <stdio.h>
 #include <stdlib.h>
+
+#ifdef _WIN32
+#define ftruncate _chsize
+#define fileno _fileno
+#endif
 
 static PyObject *Py_tcl_minify(PyObject *self, PyObject *arg) {
     Py_ssize_t size;
@@ -25,8 +31,66 @@ static PyObject *Py_tcl_minify(PyObject *self, PyObject *arg) {
     return out;
 }
 
+static PyObject *Py_tcl_minify_file(PyObject *self, PyObject *arg) {
+    const char *path = PyUnicode_AsUTF8AndSize(arg, NULL);
+    if (unlikely(path == NULL)) {
+        return NULL;
+    }
+
+    FILE *fp = fopen(path, "r+");
+    if (fp == NULL) {
+        PyErr_SetString(PyExc_OSError, "Error opening TCL file to read");
+        return NULL;
+    }
+
+    fseek(fp, 0, SEEK_END);
+    const long file_size = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+
+    if (unlikely(file_size < 0)) {
+        PyErr_SetString(PyExc_OSError, "Error telling TCL file");
+        fclose(fp);
+        return NULL;
+    }
+
+    char *source = (char *)malloc(file_size * sizeof(char));
+    if (unlikely(source == NULL)) {
+        fclose(fp);
+        return NULL;
+    }
+
+    const size_t read_bytes = fread(source, sizeof(char), file_size, fp);
+    if (unlikely(ferror(fp))) {
+        fclose(fp);
+        PyErr_SetString(PyExc_OSError, "Error reading TCL file");
+        return NULL;
+    }
+
+    size_t minified_size;
+    char *minified_source = tcl_minify(source, read_bytes, &minified_size);
+
+    if (ftruncate(fileno(fp), 0) < 0) {
+        fclose(fp);
+        PyErr_SetString(PyExc_OSError, "Error truncating TCL file");
+        return NULL;
+    }
+    rewind(fp);
+
+    const size_t written_bytes = fwrite(minified_source, sizeof(char), minified_size, fp);
+    fclose(fp);
+    free(minified_source);
+
+    if (written_bytes != minified_size) {
+        PyErr_SetString(PyExc_OSError, "Error writing TCL file");
+        return NULL;
+    }
+
+    return Py_None;
+}
+
 static PyMethodDef parse_methods[] = {
     {"tcl_minify", Py_tcl_minify, METH_O, NULL},
+    {"tcl_minify_file", Py_tcl_minify_file, METH_O, NULL},
     {NULL, NULL, 0, NULL},
 };
 
