eeb0214041448dab616dd12ceb7de13701414f7b
jbjd <jimdemetris@yahoo.com>
2026-06-27 17:35:24 -0500

    Improvement: Add -flto to compile args

--- setup.py
+++ setup.py
@@ -14,9 +14,18 @@ if os.name == "nt" and not os.path.exists("setup.cfg"):
 compiler=mingw32""")
 
 
-args: list[str] = ["-O3", "-march=native", "-mtune=native", "-Wall", "-Werror"]
+args: list[str] = [
+    "-O3",
+    "-march=native",
+    "-mtune=native",
+    "-flto",
+    "-Wall",
+    "-Werror",
+]
 
-if sys.platform == "win32" and not os.environ.get("PTCL_DISALLOW_UTF8_CHECK"):
+PTCL_DISALLOW_UTF8_CHECK: str = "PTCL_DISALLOW_UTF8_CHECK"
+
+if sys.platform == "win32" and not os.environ.get(PTCL_DISALLOW_UTF8_CHECK):
     import ctypes
 
     # Check if code page in UTF-8
@@ -25,17 +34,17 @@ if sys.platform == "win32" and not os.environ.get("PTCL_DISALLOW_UTF8_CHECK"):
             "\n-----\n"
             "WARNING: Will compile using UTF-8 file paths since it seems UTF-8 support "
             "is enabled in windows settings. If you have issues, "
-            "compile with env PTCL_DISALLOW_UTF8_CHECK set to 1"
+            f"compile with env {PTCL_DISALLOW_UTF8_CHECK} set to 1"
             "\n-----\n",
         )
         args.append("-DPTCL_UTF8")
 
 # Define the extension module
 parse_extension = Extension(
-    name=f"{PACKAGE}.parse",
+    name=PACKAGE + ".parse",
     sources=[
-        f"{C_FOLDER}/python_modules/parse.c",
-        f"{C_FOLDER}/parse.c",
+        C_FOLDER + "/python_modules/parse.c",
+        C_FOLDER + "/parse.c",
     ],
     extra_compile_args=args,
 )
